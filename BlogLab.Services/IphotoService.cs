5f7f3dd25541d6f5119c291ce362ec143411129d
kirt Registe <kirtregiste@outlook.com>
2022-11-11 19:25:36 -0600

    services project completed

---
+++ BlogLab.Services/IphotoService.cs
@@ -0,0 +1,14 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace BlogLab.Services
+{
+    public interface IphotoService
+    {
+        public Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+
+        public Task<DeletionResult> DeletePhotoAsync(string publicId);
+    }
+}
---
+++ BlogLab.Services/TokenService.cs
@@ -0,0 +1,44 @@
+﻿using BlogLab.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace BlogLab.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        private readonly string _issuer;
+                                                                                                                              
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            _issuer = config["Jwt:Issuer"];
+        }
+
+        public string createToken(ApplicationUserIdentity user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.ApplicationUserId.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName.ToString())
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                _issuer,
+                _issuer,
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
